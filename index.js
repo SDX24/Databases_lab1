6fade8df393c85138ccd224b5ecd757631002849
SDX24 <161406791+SDX24@users.noreply.github.com>
2025-01-15 22:18:34 -0800

    Changed name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3016;
 
 app.get('/', (req, res) => {
-  res.send(`Stefan Dorosh! ¯\\_(ツ)_/¯ 🐢`);
+  res.send(`Stefan Dorosh! ¯\\_(ツ)_/¯ 💀`);
 });
 
 app.listen(port, () => {
